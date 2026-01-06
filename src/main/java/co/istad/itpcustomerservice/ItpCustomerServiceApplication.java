9579a298151b4b8f7da62f6b5199e24e33d1b1b3
KungSovannda <kungsovannda@gmail.com>
2026-01-06 15:08:17 +0700

    feat: config client, discovery client

---
+++ src/main/java/co/istad/itpcustomerservice/ItpCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package co.istad.itpcustomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItpCustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItpCustomerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/istad/itpcustomerservice/ItpCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.itpcustomerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItpCustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
